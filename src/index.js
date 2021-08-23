af5384194e36b1b0dcd5d8e2b9556cbc2d4b2609
felipe reyel <felipe.reyel@gmail.com>
2021-08-22 21:48:31 -0300

    better exporting

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import Lexical  from "./lexical.js";
+import Lexical from "./lexical.js";
 // import Syntatical  from "./syntatical.js";
 
-export function compile(program) {
-    const lexical = new Lexical(program);
-    return lexical.run();
-    
-    // syntatical = new Syntatical(lexical)
-    // syntatical.parse()
-}
+const compile = (program) => {
+  const lexical = new Lexical(program);
+  return lexical.run();
+
+  // syntatical = new Syntatical(lexical)
+  // syntatical.parse()
+};
+
+export {
+  compile,
+  Lexical,
+  // Syntatical
+};
